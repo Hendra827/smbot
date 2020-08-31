b842649d930730ad5e9fabe0fc18b6ef2e787ab7
Hendra827 <31788233+Hendra827@users.noreply.github.com>
2020-08-31 11:07:26 +0700

    Add files via upload

---
+++ tiktod.js
@@ -0,0 +1,20 @@
+const TikTokScraper = require('tiktok-scraper')
+
+module.exports = async () => {
+const tiktok = (url) => new Promise((resolve, reject) => {
+    TikTokScraper.getVideoMeta(url, { noWaterMark: true, hdVideo: true })
+        .then(async (result) => {
+            console.log('Get Video From', '@' + result.authorMeta.name, 'ID:', result.id)
+            if (result.videoUrlNoWaterMark !== '') {
+                result.url = result.videoUrlNoWaterMark
+                result.NoWaterMark = true
+            } else {
+                result.url = result.videoUrl
+                result.NoWaterMark = false
+            }
+            resolve(result)
+        }).catch((err) => {
+            console.error(err)
+            reject(err)
+        })
+})
